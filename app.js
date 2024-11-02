4294e5ce5af8f614af36fdf1444c7449858accfe
Lzcom <136038607+Lzcom@users.noreply.github.com>
2024-11-02 17:21:24 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 0;
 const { addHeroe, delHeroe, updHeroe, findHeroeByCriteria, orderHeroeByCriteria } = require("./index"); 
+const { swaggerUi, swaggerDocs } = require('./config/swagger');
+const ejemploRoutes = require('./routes/Ejemplo'); 
+
+
+// Middleware para servir la documentación de Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Resto de tu configuración y rutas...
+// Usa las rutas definidas
+app.use('/api/ejemplo', ejemploRoutes); // Aquí defines el prefijo para tus rutas
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+  });
 
 app.use(express.json()); // Middleware para procesar JSON
 
@@ -26,6 +40,6 @@ app.post('/heroes/search', findHeroeByCriteria);
 app.post('/heroes/order', orderHeroeByCriteria);
 
 // Iniciar servidor
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log("Servidor corriendo en puerto", port);
 });
